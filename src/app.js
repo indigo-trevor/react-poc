61911469877809c343c132fe7fd47af6595e258d
Trevor Robinson <trevor.robinson@indigoslate.com>
2017-10-25 12:51:36 -0700

    initial setup

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// Libs
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./css/styles.scss";
+
+// Components
+import Scoreboard from './components/Scoreboard';
+
+ReactDOM.render(<Scoreboard />, document.getElementById('container'));
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+var webpackConfig = {
+	entry: "./src/app.js",
+	output: {
+		path: "build",
+		filename: "bundle.js"
+	},
+	module: {
+		loaders: [
+			{
+				loader: "babel-loader",
+				test: /\.js$/
+			},
+			{
+				loaders: ["style", "css", "sass"],
+				test: /\.scss$/
+			},
+			{
+				loaders: ["url", "img"],
+				test: /\.png$/
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "src/index.html"
+		})
+	]
+};
+
+module.exports = webpackConfig;
